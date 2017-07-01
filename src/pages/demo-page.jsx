eb0f0eaf1c5c297d95ae6d2cb721dfd3ab12727f
Mat Janson Blanchet <mat.jb@jansensan.net>
2017-06-30 21:39:22 -0400

    Added content to showcase more of the new styles

--- src/pages/demo-page.jsx
+++ src/pages/demo-page.jsx
@@ -28,22 +28,35 @@ export default class DemoPage extends Component {
         <section>
           <div className="centered-content">
             <div className="column left">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque omnis illo provident, officiis tenetur placeat debitis, consequuntur, consectetur id assumenda suscipit repellat necessitatibus rem sint laudantium deleniti incidunt atque.
-              </p>
+              <div className="column-content">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque omnis illo provident, officiis tenetur placeat debitis, consequuntur, consectetur id assumenda suscipit repellat necessitatibus rem sint laudantium deleniti incidunt atque.
+                </p>
+              </div>
             </div>
             <div className="column right">
-              <ul>
-                <li>
-                  <div className="li-label">Option 01</div>
-                </li>
-                <li>
-                  <div className="li-label">Option 02</div>
-                </li>
-                <li>
-                  <div className="li-label">Option 03</div>
-                </li>
-              </ul>
+              <div className="column-content">
+                <ul>
+                  <li>
+                    <button
+                      className="li-btn"
+                      type="button"
+                    >Button 01</button>
+                  </li>
+                  <li>
+                    <button
+                      className="li-btn"
+                      type="button"
+                    >Button 02</button>
+                  </li>
+                  <li>
+                    <button
+                      className="li-btn"
+                      type="button"
+                    >Button 03</button>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </section>
@@ -51,31 +64,38 @@ export default class DemoPage extends Component {
         <section>
           <div className="centered-content">
             <div className="column left">
-              <ul>
-                <li>
-                  <button
-                    className="li-btn"
-                    type="button"
-                  >Button 01</button>
-                </li>
-                <li>
-                  <button
-                    className="li-btn"
-                    type="button"
-                  >Button 02</button>
-                </li>
-                <li>
-                  <button
-                    className="li-btn"
-                    type="button"
-                  >Button 03</button>
-                </li>
-              </ul>
+              <div className="column-content">
+                <ul>
+                  <li>
+                    <div className="li-label">Option 01</div>
+                  </li>
+                  <li>
+                    <div className="li-label">Option 02</div>
+                  </li>
+                  <li>
+                    <div className="li-label">Option 03</div>
+                  </li>
+                  <li>
+                    <div className="li-label">Option 04</div>
+                  </li>
+                  <li>
+                    <div className="li-label">Option 05</div>
+                  </li>
+                  <li>
+                    <div className="li-label">Option 06</div>
+                  </li>
+                </ul>
+              </div>
             </div>
             <div className="column right">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque omnis illo provident, officiis tenetur placeat debitis, consequuntur, consectetur id assumenda suscipit repellat necessitatibus rem sint laudantium deleniti incidunt atque.
-              </p>
+              <div className="column-content">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque omnis illo provident, officiis tenetur placeat debitis, consequuntur, consectetur id assumenda suscipit repellat necessitatibus rem sint laudantium deleniti incidunt atque.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque omnis illo provident, officiis tenetur placeat debitis, consequuntur, consectetur id assumenda suscipit repellat necessitatibus rem sint laudantium deleniti incidunt atque.
+                </p>
+              </div>
             </div>
           </div>
         </section>
