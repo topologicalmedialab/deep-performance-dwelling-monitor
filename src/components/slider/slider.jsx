be12b8b48aeaf1db4f5416d757c77038fa52bbe6
Mat Janson Blanchet <mat.jb@jansensan.net>
2017-07-18 14:29:45 -0400

    Added touch events for touch screens

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 
 
@@ -19,6 +20,7 @@ export default class Slider extends Component {
     // dom
     this._handle = null;
     this._progressBar = null;
+    this._touchId = null;
 
     // TODO: move to state?
     this._isDragging = false;
@@ -53,12 +55,23 @@ export default class Slider extends Component {
     this._handle.addEventListener('mousedown', this.onGrabbedHandle.bind(this));
     window.onmousemove = this.onMouseMoved.bind(this);
     window.onmouseup = this.onMouseReleased.bind(this);
+
+    this._handle.addEventListener('touchstart', this.onTouchedHandle.bind(this));
+    this._handle.addEventListener('touchend', this.onTouchEnded.bind(this));
+    this._handle.addEventListener('touchmove', this.onTouchMoved.bind(this));
   }
 
 
   // methods definitions
+  dragHandle(clientX) {
+    this._dragDelta = clientX - this._dragOrigin;
+    this._handle.style.left = this.getNewHandlePosition();
+    this.updateRatio();
+    this.updateProgressBar();
+  }
+
   getHandleOrigin() {
-    var origin = this._handle.style.left;
+    let origin = this._handle.style.left;
 
     if (!origin) {
       origin = 0;
@@ -71,8 +84,8 @@ export default class Slider extends Component {
   }
 
   getNewHandlePosition() {
-    var value = this._handleOrigin + this._dragDelta;
-    var max = 304; // 352 (max width) - 48 (handle width)
+    let value = this._handleOrigin + this._dragDelta;
+    let max = 304; // 352 (max width) - 48 (handle width)
 
     if (value < 0) {
       value = 0;
@@ -83,36 +96,88 @@ export default class Slider extends Component {
     return value + 'px';
   }
 
+  getTouchWithId(touches, id) {
+    return _.find(touches, function findMatch(touch) {
+      return touch.identifier === id;
+    });
+  }
+
+  isSameTouch(touches) {
+    let soughtId = this._touchId;
+    let isSame = false;
+    _.forEach(touches, function (touch) {
+      if (touch.identifier !== soughtId) {
+        return;
+      } else {
+        isSame = true;
+      }
+    });
+    return isSame;
+  }
+
   onGrabbedHandle(event) {
-    this._isDragging = true;
-    this._handleOrigin = this.getHandleOrigin();
-    this._dragOrigin = event.clientX;
+    this.startDraggingHandle(event.clientX);
   }
 
   onMouseMoved(event) {
     if (this._isDragging) {
-      this._dragDelta = event.clientX - this._dragOrigin;
-      this._handle.style.left = this.getNewHandlePosition();
-      this.updateRatio();
-      this.updateProgressBar();
+      this.dragHandle(event.clientX);
     }
   }
 
   onMouseReleased() {
+    this.stopDragging();
+  }
+
+  onTouchedHandle(event) {
+    event.preventDefault();
+
+    this._touchId = event.changedTouches[0].identifier;
+    let touch = this.getTouchWithId(event.changedTouches, this._touchId);
+    this.startDraggingHandle(touch.clientX);
+  }
+
+  onTouchEnded(event) {
+    event.preventDefault();
+
+    let isSameTouch = this.isSameTouch(event.changedTouches);
+    if (isSameTouch) {
+      this._touchId = null;
+      this.stopDragging();
+    }
+  }
+
+  onTouchMoved(event) {
+    event.preventDefault();
+
+    let isSameTouch = this.isSameTouch(event.changedTouches);
+    if (this._isDragging && isSameTouch) {
+      let touch = this.getTouchWithId(event.changedTouches, this._touchId);
+      this.dragHandle(touch.clientX);
+    }
+  }
+
+  startDraggingHandle(dragOrigin) {
+    this._isDragging = true;
+    this._handleOrigin = this.getHandleOrigin();
+    this._dragOrigin = dragOrigin;
+  }
+
+  stopDragging() {
     if (this._isDragging) {
       this._isDragging = false;
     }
   }
 
   updateProgressBar() {
-    var newWidth = this._ratio * 304; // ratio * max width (304)
+    let newWidth = this._ratio * 304; // ratio * max width (304)
     this._progressBar.style.width = newWidth + 'px';
   }
 
   updateRatio() {
-    var position = parseInt(this._handle.style.left.replace('px', ''), 10);
-    var max = 304; // 352 (max width) - 48 (handle width)
-    var newRatio = position / max;
+    let position = parseInt(this._handle.style.left.replace('px', ''), 10);
+    let max = 304; // 352 (max width) - 48 (handle width)
+    let newRatio = position / max;
     if (newRatio !== this._ratio) {
       this._ratio = position / max;
       if (this.props.onUpdated) {
